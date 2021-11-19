c77bfb32df5ccb9a9fe341b5c1599cf17064c537
Iván Neira <ien1983@gmail.com>
2021-11-19 09:03:38 -0300

    sin switch

--- resources/views/rutinas/index.blade.php
+++ resources/views/rutinas/index.blade.php
@@ -62,14 +62,7 @@
                         </div>
 
                         <div class="col-md-12">
-                            {{-- 
-                            <div class="form-group">
-                                <div class="custom-control custom-switch">
-                                    <input type="checkbox" class="custom-control-input" name="active" id="active" checked tabindex="7">
-                                    <label class="custom-control-label" for="active">Inactivo / Activo</label>
-                                </div>
-                            </div>
-                            --}}
+  
                         </div>
 
                     </div>
